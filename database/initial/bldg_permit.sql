54b7af2e86dfc8561174ad0c3c3141c75f8b38e7
Mark Kimsal <metrofindings@gmail.com>
2021-02-21 16:31:38 -0500

    initial data dumps

---
+++ database/initial/bldg_permit.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2ded7b74c8940ca2fd37652d80a10c441a4376ef9b5b592867ca8f9f6f52dcc1
+size 18890379
---
+++ database/initial/zoning_land_use.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:932e3b7006ac2a3eeb95b2a92e77450c2237a9b5b340bf2b57e55d2b0c2f473a
+size 96027711
