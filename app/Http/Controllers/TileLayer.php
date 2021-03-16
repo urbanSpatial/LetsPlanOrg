e752c055e530d0e158f11203fd6bd5e20497f1c4
Mark Kimsal <metrofindings@gmail.com>
2021-03-16 15:59:31 -0400

    chore: lint

--- app/Http/Controllers/TileLayer.php
+++ app/Http/Controllers/TileLayer.php
@@ -6,33 +6,34 @@ use Illuminate\Http\Request;
 
 class TileLayer extends Controller
 {
-	public function index(Request $request, $layer, $z, $x, $y) {
-      $flip = true;
-      if ($flip) {
-        $y = pow(2, $z) - 1 - $y;
-      }
+    public function index(Request $request, $layer, $z, $x, $y)
+    {
+        $flip = true;
+        if ($flip) {
+            $y = pow(2, $z) - 1 - $y;
+        }
 
-      $result =\DB::connection('mbtiles')
-          ->table('tiles')
-          ->select('tile_data')
-          ->where('zoom_level', $z )
-          ->where('tile_column', $x )
-          ->where('tile_row', $y)
-          ->first();
+        $result = \DB::connection('mbtiles')
+            ->table('tiles')
+            ->select('tile_data')
+            ->where('zoom_level', $z)
+            ->where('tile_column', $x)
+            ->where('tile_row', $y)
+            ->first();
 
-      if (!$result) {
-          return response('', 404);
-      }
-      $headers = [
-          'Content-Encoding' => 'gzip',
-          'Content-Type' => 'application/x-protobuf',
-          //'Content-Type' => 'text/html',
-          'Access-Control-Allow-Origin' => '*',
-      ];
-      foreach ($headers as $_key => $_val) {
-          header($_key.': '. $_val);
-      }
-      echo $result->tile_data;
-      exit();
+        if (!$result) {
+            return response('', 404);
+        }
+        $headers = [
+            'Content-Encoding' => 'gzip',
+            'Content-Type' => 'application/x-protobuf',
+            //'Content-Type' => 'text/html',
+            'Access-Control-Allow-Origin' => '*',
+        ];
+        foreach ($headers as $_key => $_val) {
+            header($_key . ': ' . $_val);
+        }
+        echo $result->tile_data;
+        exit();
     }
 }
