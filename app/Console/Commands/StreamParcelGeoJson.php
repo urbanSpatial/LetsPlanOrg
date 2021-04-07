699cb78367dbf05aae08f2428ba614bed65f3407
Mark Kimsal <metrofindings@gmail.com>
2021-04-07 10:51:28 -0400

    fix: real estate transfer price limit, deed type

--- app/Console/Commands/StreamParcelGeoJson.php
+++ app/Console/Commands/StreamParcelGeoJson.php
@@ -30,8 +30,8 @@ class StreamParcelGeoJson extends Command
     {
         $table = $this->argument('project_table');
         $parcels = \DB::table($table)
-            ->select([$table.'.*', 'zoning_land_use.landuse', 'zoning_land_use.bldg_desc'])
-            ->leftJoin('atlas_data', 'atlas_data.parcel_id', $table.'.parcel_id')
+            ->select([$table . '.*', 'zoning_land_use.landuse', 'zoning_land_use.bldg_desc'])
+            ->leftJoin('atlas_data', 'atlas_data.parcel_id', $table . '.parcel_id')
             ->leftJoin('zoning_land_use', 'zoning_land_use.opa_account_num', 'atlas_data.opa_account_num')
             ->cursor();
         echo '{
@@ -47,10 +47,11 @@ class StreamParcelGeoJson extends Command
             $geoJson = json_decode($parcel->geo_json, true);
             $salePrice = RealEstateTx::leftJoin('atlas_data', 'atlas_data.opa_account_num', 'real_estate_tx.opa_account_num')
                 ->where('atlas_data.parcel_id', $parcel->parcel_id)
+                ->where('real_estate_tx.sale_price_adj', '>', 0)
                 ->orderBy('sale_date', 'DESC')
                 ->first();
             $feature = [
-                'type'=>'Feature',
+                'type' => 'Feature',
                 'properties' => [
                     'landuse'  => $parcel->landuse,
                     'bldg_desc' => $parcel->bldg_desc,
