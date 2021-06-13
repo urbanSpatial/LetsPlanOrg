03e180243235d2291188d3f436a1fc63e57f1631
Kent Russell <kent.russell@timelyportfolio.com>
2021-06-13 15:29:21 -0500

    lint

--- resources/js/Shared/ParcelInfo.vue
+++ resources/js/Shared/ParcelInfo.vue
@@ -1,67 +1,67 @@
 <template>
   <div>
     <v-progress-circular
-      v-if="loading"
-      class="ma-5"
-      color="grey"
+      v-if='loading'
+      class='ma-5'
+      color='grey'
       indeterminate
     />
 
-    <div v-if="!loading">
-      <h1 class="popup__title pink--text text--darken-1">
+    <div v-if='!loading'>
+      <h1 class='popup__title pink--text text--darken-1'>
         {{ parcel.address_1 }}
       </h1>
 
-      <div class="popup__subtitle text--secondary">
+      <div class='popup__subtitle text--secondary'>
         {{ parcel.property_type }}
       </div>
 
-      <div class="figure-group">
+      <div class='figure-group'>
         <parcel-figure
-          name="Built"
-          :value="parcel.year_built"
+          name='Built'
+          :value='parcel.year_built'
         />
         <parcel-figure
-          name="Zoning"
-          :value="landUse"
+          name='Zoning'
+          :value='landUse'
         />
         <parcel-figure
-          name="Permits"
-          :value="parcel.total_permits"
-        />
-      </div>
-
-      <div class="figure-group">
-        <parcel-figure
-          name="Last Sale"
-          :value="formattedSalePrice"
-        />
-        <parcel-figure
-          name="Sale Date"
-          :value="parcel.sale_year"
+          name='Permits'
+          :value='parcel.total_permits'
         />
       </div>
 
-      <div class="figure-group -scores pb-0">
+      <div class='figure-group'>
         <parcel-figure
-          name="Preservation"
-          value="0.5"
-          content-class="teal--text"
+          name='Last Sale'
+          :value='formattedSalePrice'
         />
         <parcel-figure
-          name="Community"
-          value="0.5"
-          content-class="purple--text"
+          name='Sale Date'
+          :value='parcel.sale_year'
+        />
+      </div>
+
+      <div class='figure-group -scores pb-0'>
+        <parcel-figure
+          name='Preservation'
+          value='0.5'
+          content-class='teal--text'
         />
         <parcel-figure
-          name="Variance"
-          value="0.5"
-          content-class="orange--text"
+          name='Community'
+          value='0.5'
+          content-class='purple--text'
         />
         <parcel-figure
-          name="Development"
-          value="0.5"
-          content-class="lime--text text--darken-2"
+          name='Variance'
+          value='0.5'
+          content-class='orange--text'
+        />
+        <parcel-figure
+          name='Development'
+          value='0.5'
+          content-class='lime--text text--darken-2'
         />
       </div>
     </div>
@@ -83,48 +83,48 @@ export default {
       parcel: {},
       loading: false,
       land_use_lookup: {
-        "12": "Special",
-        "2002": "Special",
-        "RSD1": "Low Res",
-        "RSD2": "Low Res",
-        "RSD3": "Low Res",
-        "RMX1": "Low Res",
-        "RMX2": "Low Res",
-        "RSA": "Low Res",
-        "RSA1": "Low Res",
-        "RSA2": "Low Res",
-        "RSA3": "Low Res",
-        "RS3": "Low Res",
-        "RSA4": "Low Res",
-        "RSA5": "Low Res",
-        "RTA1": "Low Res",
-        "RM1": "Hi Res",
-        "RM2": "Hi Res",
-        "RM3": "Hi Res",
-        "RM4": "Hi Res",
-        "RMX3": "Hi Res",
-        "IRMX": "Industrial",
-        "ICMX": "Industrial",
-        "I1": "Industrial",
-        "I2": "Industrial",
-        "I3": "Industrial",
-        "IP": "Industrial",
-        "CMX1": "Low Com",
-        "CMX2": "Low Com",
-        "CMX25": "Low Com",
-        "CMX3": "Hi Com",
-        "CMX4": "Hi Com",
-        "CMX5": "Hi Com",
-        "CA1": "Special",
-        "CA2": "Special",
-        "SPINS": "Special",
-        "SPENT": "Special",
-        "SPSTA": "Special",
-        "SPPOP": "Special",
-        "SPPOA": "Special",
-        "SPAIR": "Special",
-        "SC": "Special",
-        "SP": "Special"
+        12: 'Special',
+        2002: 'Special',
+        RSD1: 'Low Res',
+        RSD2: 'Low Res',
+        RSD3: 'Low Res',
+        RMX1: 'Low Res',
+        RMX2: 'Low Res',
+        RSA: 'Low Res',
+        RSA1: 'Low Res',
+        RSA2: 'Low Res',
+        RSA3: 'Low Res',
+        RS3: 'Low Res',
+        RSA4: 'Low Res',
+        RSA5: 'Low Res',
+        RTA1: 'Low Res',
+        RM1: 'Hi Res',
+        RM2: 'Hi Res',
+        RM3: 'Hi Res',
+        RM4: 'Hi Res',
+        RMX3: 'Hi Res',
+        IRMX: 'Industrial',
+        ICMX: 'Industrial',
+        I1: 'Industrial',
+        I2: 'Industrial',
+        I3: 'Industrial',
+        IP: 'Industrial',
+        CMX1: 'Low Com',
+        CMX2: 'Low Com',
+        CMX25: 'Low Com',
+        CMX3: 'Hi Com',
+        CMX4: 'Hi Com',
+        CMX5: 'Hi Com',
+        CA1: 'Special',
+        CA2: 'Special',
+        SPINS: 'Special',
+        SPENT: 'Special',
+        SPSTA: 'Special',
+        SPPOP: 'Special',
+        SPPOA: 'Special',
+        SPAIR: 'Special',
+        SC: 'Special',
+        SP: 'Special'
       }
     };
   },
