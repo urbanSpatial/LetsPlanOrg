d073c304f9e6436ef5c37afe42d227c11680b2a5
Nico Westerdale <nico@iconico.com>
2021-06-25 13:22:12 -0400

    [LP-79] Leaflet css
    fixes the map layout in the survey

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
 import { App, plugin } from '@inertiajs/inertia-vue';
 import { InertiaProgress } from '@inertiajs/progress';
 import Layout from './Shared/Layouts/Layout.vue';
@@ -21,6 +23,18 @@ InertiaProgress.init();
 Vue.mixin({ methods: { route } });
 
 const el = document.getElementById('app');
+
+const iconRetinaUrl = require('leaflet/dist/images/marker-icon-2x.png');
+const iconUrl = require('leaflet/dist/images/marker-icon.png');
+const shadowUrl = require('leaflet/dist/images/marker-shadow.png');
+
+// eslint-disable-next-line no-underscore-dangle
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+});
 
 new Vue({
   vuetify,
